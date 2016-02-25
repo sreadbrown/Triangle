53691c1ddb44dcfae3e9b3b1f13f8b4ec6957d2e
Guest User <student@epicodus.com>
2016-02-24 20:34:32 -0800

    complete the first part of the triangle tracker problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,16 @@
 var triangletracker = function(a,b,c) {
-  // if (((a+b) <= c) || ((b+c) <= a) || ((a+c) <= b)) {
-  //   return "not a triangle"
-  // }
-
-  // if ((a === b) && (b === c)) {
-  //   return "equilateral"
-  // }
-
-  // if ((a === b) && (a !== c) || (b === c) && (b !== a) || (c === a) && (a !== b)) {
-  //   return "isoceles"
-  // }
-
-  if (true) {
-
+  if (((a+b) <= c) || ((b+c) <= a) || ((a+c) <= b)) {
+    return "not a triangle"
   }
-
+  else if ((a === b) && (b === c)) {
+    return "equilateral"
+  } else if ((a === b) && (a !== c) || (b === c) && (b !== a) || (c === a) && (a !== b)) {
+    return "isoceles"
+  } else if((a !== b) && (b !== c)) {
+    return "scalene"
+    }
 };
+
 
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
